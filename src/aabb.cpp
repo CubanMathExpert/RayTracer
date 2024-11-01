b7156dd41ce1efdc02e30770eba51915924facf7
CubanMathExpert <rafaelglez98@hotmail.com>
2024-11-01 16:38:57 -0400

    retrieve_corners done

--- src/aabb.cpp
+++ src/aabb.cpp
@@ -9,7 +9,21 @@ bool AABB::intersect(Ray ray, double t_min, double t_max)  {
 // @@@@@@ VOTRE CODE ICI
 // Implémenter la fonction qui permet de trouver les 8 coins de notre AABB.
 std::vector<double3> retrieve_corners(AABB aabb) {
-	return std::vector<double3>{};
+	std::vector<double3> boxCorners;
+
+	double3 min = aabb.min;
+	double3 max = aabb.max;
+
+	boxCorners[0] = double3{min.x, min.y, min.z};
+	boxCorners[1] = double3{min.x, min.y, max.z};
+	boxCorners[2] = double3{min.x, max.y, min.z};
+	boxCorners[3] = double3{min.x, max.y, max.z};
+	boxCorners[4] = double3{max.x, min.y, min.z};
+	boxCorners[5] = double3{max.x, min.y, max.z};
+	boxCorners[6] = double3{max.x, max.y, min.z};
+	boxCorners[7] = double3{max.x, max.y, max.z};
+
+	return boxCorners;
 };
 
 // @@@@@@ VOTRE CODE ICI
